efc4170c2696e4bed1944714ecc6939c8d39cfd4
Jerry Frederic <jerry.fpl@gmail.com>
2021-10-07 14:40:56 +0200

    fix(about): change style loader to sass + script StackAnimate
    fix rendering bug on about page

--- scripts/circleStackAnimate.js
+++ scripts/circleStackAnimate.js
@@ -1,18 +1,18 @@
 //Web development stacks
-let circular = new CircleType(document.getElementById("circular"));
+var circular = new CircleType(document.getElementById("circular"));
 circular.radius(180);
 
 //Tools & platforms
-let circular2 = new CircleType(
+var circular2 = new CircleType(
   document.getElementById("circular-tools-platforms")
 );
 circular2.radius(180);
 
-let circular3 = new CircleType(
+var circular3 = new CircleType(
   document.getElementById("circular-tools-platforms-2")
 );
 circular3.radius(160);
 
 //Design
-let circular4 = new CircleType(document.getElementById("circular-design"));
+var circular4 = new CircleType(document.getElementById("circular-design"));
 circular4.radius(90);
--- src/about.js
+++ src/about.js
@@ -1,3 +1,2 @@
 import "./sections/about/about.html";
-import "../assets/styles/normalize.css";
-import "./sections/about/about.css";
+import "./sections/about/about.scss";
