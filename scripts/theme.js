933be2a30d017c5533901ad9d6437d1c008de3db
Jerry Frederic <jfpl.dev@gmail.com>
2021-10-23 11:56:06 +0200

    feat(app): set dark mode
    only works on main page
    
    need to handle with localStorage or cookie

--- scripts/theme.js
+++ scripts/theme.js
@@ -24,3 +24,19 @@ icon.onclick = function () {
     localStorage.setItem("theme", "light");
   }
 };
+
+// Récupération du champ à enregistrer
+var champ = document.getElementById("icon");
+
+// Vérification de l'existence d'une donnée enregistrée auparavant
+// (ce ne sera le cas que si la page a été rafraîchie)
+if (sessionStorage.getItem("dark-theme")) {
+  // Restauration du contenu du champ
+  champ.value = sessionStorage.getItem("dark-theme");
+}
+
+// Écoute des changements de valeur du champ
+champ.addEventListener("click", function () {
+  // Enregistrement de la saisie utilisateur dans le stockage de session
+  sessionStorage.setItem("dark-theme", champ.value);
+});
