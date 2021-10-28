a77767a80886443a6bc2e3254cb596f5d9d1d154
Jerry Frederic <jfpl.dev@gmail.com>
2021-10-28 06:35:28 +0200

    feat(app): set scrollreveal

---
+++ scripts/revealOnScroll.js
@@ -0,0 +1,13 @@
+ScrollReveal().reveal(".stack", { duration: 2000 });
+ScrollReveal().reveal(".about", { duration: 1000 });
+ScrollReveal().reveal(".timeline", { duration: 3000 });
+ScrollReveal().reveal(".web-development", { duration: 2000 });
+ScrollReveal().reveal(".platforms-tools", { duration: 2000 });
+ScrollReveal().reveal(".stacks-small-screen", { duration: 2000 });
+ScrollReveal().reveal(".tools-and-platforms-small-screen", { duration: 2000 });
+ScrollReveal().reveal(".design-tools-small-screen", { duration: 2000 });
+ScrollReveal().reveal(".get-in-touch", { duration: 2000 });
+ScrollReveal().reveal(".footer", { duration: 2000 });
+ScrollReveal().reveal(".tab", { interval: 200 });
+
+ScrollReveal(options);
