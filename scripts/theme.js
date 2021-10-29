6893b046541cc3969c371c4d628b9f0b5e657137
Jerry Frederic <jfpl.dev@gmail.com>
2021-10-29 23:52:04 +0200

    build(app): set Dark Mode

--- scripts/theme.js
+++ scripts/theme.js
@@ -7,20 +7,20 @@ if (window.localStorage.getItem("theme") == null) {
 let localData = window.localStorage.getItem("theme");
 
 if (localData == "light") {
-  icon.src = "../dist/images/moon.png";
+  icon.src = "../assets/images/moon.png";
   document.body.classList.remove("dark-theme");
 } else if (localData == "dark") {
-  icon.src = "../dist/images/moon.png";
+  icon.src = "../assets/images/sun.png";
   document.body.classList.add("dark-theme");
 }
 
 icon.onclick = function () {
   document.body.classList.toggle("dark-theme");
   if (document.body.classList.contains("dark-theme")) {
-    icon.src = "../dist/images/sun.png";
+    icon.src = "../assets/images/sun.png";
     localStorage.setItem("theme", "dark");
   } else {
-    icon.src = "../dist/images/moon.png";
+    icon.src = "../assets/images/moon.png";
     localStorage.setItem("theme", "light");
   }
 };
