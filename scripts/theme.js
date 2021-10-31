d3644642b30db190678a911a3bc7ae6f4bd90809
Jerry Frederic <jfpl.dev@gmail.com>
2021-10-31 23:07:35 +0100

    build(app): set Dark Mode

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,26 +1,26 @@
-// var icon = document.getElementById("icon");
+var icon = document.getElementById("icon");
 
-// if (window.localStorage.getItem("theme") == null) {
-//   window.localStorage.setItem("theme", "light");
-// }
+if (window.localStorage.getItem("theme") == null) {
+  window.localStorage.setItem("theme", "light");
+}
 
-// let localData = window.localStorage.getItem("theme");
+let localData = window.localStorage.getItem("theme");
 
-// if (localData == "light") {
-//   icon.src = "/55eae93a51e7d7593742.png";
-//   document.body.classList.remove("dark-theme");
-// } else if (localData == "dark") {
-//   icon.src = "../dist/assets/images/sun.png";
-//   document.body.classList.add("dark-theme");
-// }
+if (localData == "light") {
+  icon.src = "../src/images/moon.png";
+  document.body.classList.remove("dark-theme");
+} else if (localData == "dark") {
+  icon.src = "../src/images/moon.png";
+  document.body.classList.add("dark-theme");
+}
 
-// icon.onclick = function () {
-//   document.body.classList.toggle("dark-theme");
-//   if (document.body.classList.contains("dark-theme")) {
-//     icon.src = "../dist/assets/images/sun.png";
-//     localStorage.setItem("theme", "dark");
-//   } else {
-//     icon.src = "/55eae93a51e7d7593742.png";
-//     localStorage.setItem("theme", "light");
-//   }
-// };
+icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "../src/images/sun.png";
+    localStorage.setItem("theme", "dark");
+  } else {
+    icon.src = "../src/images/moon.png";
+    localStorage.setItem("theme", "light");
+  }
+};
