0907558d20a7802b2f19f67db924015f77c9ab57
Jerry Frederic <jfpl.dev@gmail.com>
2021-11-03 14:07:30 +0100

    style(app):fix prefered-color-scheme
    trouble occured with prefered color scheme when setting dark/light mode

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,26 +1,26 @@
-// var icon = document.getElementById("icon");
+var icon = document.getElementById("icon");
 
-// if (window.localStorage.getItem("theme") == null) {
-//   window.localStorage.setItem("theme", "light");
-// }
+if (window.localStorage.getItem("theme") == null) {
+  window.localStorage.setItem("theme", "light");
+}
 
-// let localData = window.localStorage.getItem("theme");
+let localData = window.localStorage.getItem("theme");
 
-// if (localData == "light") {
-//   icon.src = "/images/moon.png";
-//   document.body.classList.remove("dark-theme");
-// } else if (localData == "dark") {
-//   icon.src = "/images/moon.png";
-//   document.body.classList.add("dark-theme");
-// }
+if (localData == "light") {
+  icon.src = "/images/moon.png";
+  document.body.classList.remove("dark-theme");
+} else if (localData == "dark") {
+  icon.src = "/images/moon.png";
+  document.body.classList.add("dark-theme");
+}
 
-// icon.onclick = function () {
-//   document.body.classList.toggle("dark-theme");
-//   if (document.body.classList.contains("dark-theme")) {
-//     icon.src = "/images/sun.png";
-//     localStorage.setItem("theme", "dark");
-//   } else {
-//     icon.src = "/images/moon.png";
-//     localStorage.setItem("theme", "light");
-//   }
-// };
+icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "/images/sun.png";
+    localStorage.setItem("theme", "dark");
+  } else {
+    icon.src = "/images/moon.png";
+    localStorage.setItem("theme", "light");
+  }
+};
