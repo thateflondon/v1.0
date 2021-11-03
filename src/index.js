9055066944d1b4236b5cdf739590a02c932f2c7a
Jerry Frederic <jfpl.dev@gmail.com>
2021-11-03 13:48:09 +0100

    style(app):fix prefered-color-scheme
    trouble occured with prefered color scheme when setting dark/light mode

--- src/index.js
+++ src/index.js
@@ -1,17 +1,2 @@
 import "../index.html";
 import "../assets/styles/style.scss";
-
-// import moonImage from "../assets/images/moon.png";
-// import sunImage from "../assets/images/sun.png";
-
-// var moonImg = document.getElementsByClassName("themeBtn");
-// moonImg.src = moonImage;
-
-// var sunImg = document.getElementsByClassName("themeBtn");
-// sunImg.src = sunImage;
-
-// import moonImage from "../assets/images/moon.png";
-// import sunImage from "../assets/images/sun.png";
-
-// img.src = moonImage;
-// img.src = sunImage;
