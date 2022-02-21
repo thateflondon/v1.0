01d3eb5a8b357e0cabc237563582d60643d15483
Jerry Frederic <jfpl.dev@gmail.com>
2022-02-21 23:19:48 +0100

    bugfix(app): update webpack config
    contentBase depreciated by webpack team

--- webpack.config.js
+++ webpack.config.js
@@ -211,9 +211,16 @@ module.exports = {
   },
 
   // required if using webpack-dev-server
+  // devServer: {
+  //   contentBase: path.join(__dirname, "dist"),
+  //   port: 9000,
+  //   hot: true,
+  // },
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
     port: 9000,
-    hot: true,
   },
 };
