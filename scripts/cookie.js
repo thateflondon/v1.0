9fe71054d0bf8f07aa73cefea17c09a5b077cafc
Jerry Frederic <jfpl.dev@gmail.com>
2022-04-08 05:52:17 +0200

    fix(app): set expiration date on cookie consent
    set timeout on cookie with cookie.set() method
    
    with the previous build the cookie consent modal was called each time the index page was refresh or recall

--- scripts/cookie.js
+++ scripts/cookie.js
@@ -11,12 +11,15 @@ let acceptCookieBtn = document.querySelector(".button__accept");
 cancelCookieBtn.addEventListener("click", function () {
   //   alert(0);
   cookieConsent.classList.remove("active");
+  localStorage.setItem("cookieApprouved", "no");
+  Cookies.set("cookieApprouved", "false", { expires: 1 });
 });
 
 acceptCookieBtn.addEventListener("click", function () {
   cookieConsent.classList.remove("active");
   //store the cookie in localStorage
   localStorage.setItem("cookieApprouved", "yes");
+  Cookies.set("cookieApprouved", "true", { expires: 1 });
 });
 
 // Set timeout
@@ -24,7 +27,10 @@ setTimeout(function () {
   //store the cookie in localStorage
   let cookieApprouved = localStorage.getItem("cookieApprouved");
   //manage the active class depending if cookie is set or not
-  if (cookieApprouved != "yes") {
+  if (!cookieApprouved) {
     cookieConsent.classList.add("active");
   }
+  // if (cookieApprouved == "no") {
+  //   cookieConsent.classList.remove("active");
+  // }
 }, 2000);
